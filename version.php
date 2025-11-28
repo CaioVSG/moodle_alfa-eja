78891092b829b14d7b0ad25d43e9638e831b4267
Caio <caio.vsg@hotmail.com>
2025-11-28 11:32:20 -0300

    update: version

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 
 $plugin->component = 'theme_alfa';
-$plugin->version = 202511250000;
+$plugin->version = 202511280000;
 $plugin->requires = 2022041900;
 $plugin->dependencies = [
     'theme_boost' => ANY_VERSION
