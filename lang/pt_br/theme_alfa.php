39e5fccdc7b66a82b3f91dd30e1dba2c2079d24d
Caio <caio.vsg@hotmail.com>
2025-11-28 11:43:47 -0300

    fix: erro de sintaxe

--- lang/pt_br/theme_alfa.php
+++ lang/pt_br/theme_alfa.php
@@ -1,60 +1,49 @@
 <?php
-// Arquivo de idioma personalizado para o tema Alfa.
-
-$string['loginto'] = 'Acesso (customizado pelo tema alfa)';
-$string['banner']      = 'Banner';
-$string['banner_desc'] = 'Espaço destinado a exibir destaques ou informes importantes sobre a plataforma EAD.';
-
-// Sobrescrever strings do core do Moodle
-$string['availablecourses'] = 'Todos os cursos';
-
-// --- Strings para Configurações Avançadas ---
-$string['advancedsettings'] = 'Configurações avançadas';
-$string['rawscss_pre'] = 'SCSS puro inicial';
-$string['rawscss_pre_desc'] = 'Neste campo você pode fornecer código SCSS inicial para definir variáveis.';
-$string['rawscss'] = 'SCSS puro';
-$string['rawscss_desc'] = 'Use este campo para fornecer código SCSS ou CSS que será inserido no final da folha de estilos.';
-
-<?php
 // Arquivo: /theme/alfa/lang/pt_br/theme_alfa.php
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Tema Alfa'; // Nome do plugin na lista
+$string['pluginname'] = 'Tema Alfa';
 
-// --- Strings usadas no settings.php (que estavam faltando) ---
-
-// Título da página de configurações
+// --- Título Principal ---
 $string['configtitle'] = 'Configurações do Tema Alfa';
 
-// Aba: Geral
-$string['navbar'] = 'Geral e Navegação'; 
+// --- Aba Geral ---
+$string['navbar'] = 'Geral e Navegação';
 $string['loginwelcome'] = 'Mensagem de boas-vindas';
-$string['loginwelcome_desc'] = 'Texto exibido na página de login acima do formulário.';
+$string['loginwelcome_desc'] = 'Texto exibido na página de login.';
 $string['navcolor'] = 'Cor da barra de navegação';
-$string['navcolor_desc'] = 'Defina a cor principal do topo (ex: #008843).';
+$string['navcolor_desc'] = 'Defina a cor principal do topo.';
 
-// Aba: Página Inicial
+// --- Aba Homepage ---
 $string['homepage'] = 'Página Inicial';
 $string['homecontent'] = 'Conteúdo da Home';
-$string['homecontent_desc'] = 'Adicione o texto ou HTML para o corpo da página inicial.';
+$string['homecontent_desc'] = 'Conteúdo principal da página inicial.';
 
-// Aba: Cadastro
+// --- Aba Cadastro ---
 $string['register'] = 'Cadastro';
 $string['registerinfo'] = 'Informações de Cadastro';
-$string['registerinfo_desc'] = 'Instruções exibidas para usuários que vão se registrar.';
+$string['registerinfo_desc'] = 'Instruções para novos usuários.';
 
-// Aba: Rodapé
+// --- Aba Rodapé ---
 $string['footer'] = 'Rodapé';
 $string['footertext'] = 'Texto do Rodapé';
-$string['footertext_desc'] = 'Texto de copyright ou informações da instituição.';
+$string['footertext_desc'] = 'Texto de copyright.';
 
-// Aba: Extras
+// --- Aba Extras ---
 $string['extra'] = 'Extras';
 $string['enablebacktotop'] = 'Botão Voltar ao Topo';
-$string['enablebacktotop_desc'] = 'Se marcado, exibe uma seta para subir a página rapidamente.';
+$string['enablebacktotop_desc'] = 'Habilita a seta de subir ao topo.';
 
-// --- Strings antigas que já estavam lá ---
-$string['loginto'] = 'Acesso (customizado pelo tema alfa)';
+// --- Aba Avançado ---
+$string['advancedsettings'] = 'Configurações Avançadas';
+$string['rawscss_pre'] = 'SCSS puro inicial';
+$string['rawscss_pre_desc'] = 'Código SCSS para definir variáveis (executado antes).';
+$string['rawscss'] = 'SCSS puro';
+$string['rawscss_desc'] = 'Código CSS/SCSS personalizado (executado no final).';
+
+// --- Strings antigas ---
+$string['loginto'] = 'Acesso';
 $string['banner'] = 'Banner';
-$string['banner_desc'] = 'Espaço destinado a exibir destaques ou informes importantes sobre a plataforma EAD.';
+$string['banner_desc'] = 'Descrição do banner.';
+$string['availablecourses'] = 'Todos os cursos';
