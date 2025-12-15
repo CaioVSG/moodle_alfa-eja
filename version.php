ee22866780620ac140d03ed3a604bda004ccd385
Caio <caio.vsg@hotmail.com>
2025-12-15 13:45:34 -0300

    feat: modificações no footer

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 
 $plugin->component = 'theme_alfa';
-$plugin->version = 202512110000;
+$plugin->version = 202512150000;
 $plugin->requires = 2022041900;
 $plugin->dependencies = [
     'theme_boost' => ANY_VERSION
