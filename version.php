5fa565d3cc4ae683713a4eab5e09d0652375b8fc
Caio <caio.vsg@hotmail.com>
2025-12-15 14:24:04 -0300

    fix: imagem do footer

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 
 $plugin->component = 'theme_alfa';
-$plugin->version = 202512150000;
+$plugin->version = 202512151000;
 $plugin->requires = 2022041900;
 $plugin->dependencies = [
     'theme_boost' => ANY_VERSION
