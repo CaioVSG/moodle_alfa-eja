b0e13349a1da244331430946af21624b9ebab94d
Caio <caio.vsg@hotmail.com>
2026-04-17 14:54:31 -0300

    feat: mudanças no footer, cor do titulo principal dos cursos e configuraçoes de idioma

--- lang/en/theme_alfa.php
+++ lang/en/theme_alfa.php
@@ -1,5 +1,44 @@
 <?php
+defined('MOODLE_INTERNAL') || die();
+
 $string['pluginname'] = 'Alfa EJA Theme';
-$string['banner']      = 'Banner';
-$string['banner_desc'] = 'Espaço destinado a exibir destaques ou informes importantes sobre a plataforma EAD.';
-$string['availablecourses'] = 'Todos os cursos';
+$string['configtitle'] = 'Alfa theme settings';
+
+// General and navigation.
+$string['navbar'] = 'General and navigation';
+$string['loginwelcome'] = 'Welcome message';
+$string['loginwelcome_desc'] = 'Text displayed on the login page.';
+$string['navcolor'] = 'Navigation bar color';
+$string['navcolor_desc'] = 'Set the main top bar color.';
+
+// Homepage.
+$string['homepage'] = 'Homepage';
+$string['homecontent'] = 'Homepage content';
+$string['homecontent_desc'] = 'Main content shown on the homepage.';
+
+// Registration.
+$string['register'] = 'Registration';
+$string['registerinfo'] = 'Registration information';
+$string['registerinfo_desc'] = 'Instructions for new users.';
+
+// Footer.
+$string['footer'] = 'Footer';
+$string['footertext'] = 'Footer text';
+$string['footertext_desc'] = 'Copyright text.';
+
+// Extra.
+$string['extra'] = 'Extra';
+$string['enablebacktotop'] = 'Back to top button';
+$string['enablebacktotop_desc'] = 'Enable the back-to-top arrow.';
+
+// Advanced settings.
+$string['advancedsettings'] = 'Advanced settings';
+$string['rawscss_pre'] = 'Raw initial SCSS';
+$string['rawscss_pre_desc'] = 'SCSS code for variables (loaded before the preset).';
+$string['rawscss'] = 'Raw SCSS';
+$string['rawscss_desc'] = 'Custom CSS/SCSS code (loaded last).';
+
+// Existing theme strings.
+$string['banner'] = 'Banner';
+$string['banner_desc'] = 'Space for highlights or important announcements about the EAD platform.';
+$string['availablecourses'] = 'All courses';
